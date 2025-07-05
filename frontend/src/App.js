ff84b8199c690cde1c47bbf36cd63a603ed60601
harsha4p28 <harsha4p28@gmail.com>
2025-07-05 15:59:07 +0530

    Initialised Frontend and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home/Home';
+import Dashboard from "./Dashboard/Dashboard"
+import Profile from "./Profile/Profile"
+import Login from "./Login/Login"
+import Register from "./Register/Register"
+import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+  const [userLoggedin, setUserLoggedin] = useState(false);
+  const [loading, setLoading] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Routes>
+          <Route path='/' element={userLoggedin ? <Dashboard /> : <Home  />} />
+          <Route path='/profile' element={<Profile />} ></Route>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path="*" element={<Navigate to='/' />} />
+       </Routes>
     </div>
   );
 }
---
+++ frontend/src/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navBarComponent'>
+        <div className='navBarMainComponent'>
+            <div className='navTitle'>
+                To Do Board
+            </div>
+            <div>
+                Home
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
